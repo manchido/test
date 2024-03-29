a2803b98faa25af83ad6e1a278bb5df75bb3bdbc
Samuel <samuel.adesina@osapiens.com>
2024-03-29 18:05:56 -0400

    Update

--- Server/models/user.js
+++ Server/models/user.js
@@ -81,12 +81,6 @@ const userSchema = new mongoose.Schema({
   },
   mobile: {
     type: String,
-    validate: {
-      validator: function (v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v)
-      },
-      message: (props) => `${props.value} is not a valid phone number!`
-    },
     required: [true, 'User phone number required']
   },
   invalidatedTokens: [String],
